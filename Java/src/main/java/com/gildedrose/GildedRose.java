be381eae3257176af55e6abffa6aebb37fe9bdf2
John Hendrikx <hjohn@xs4all.nl>
2019-05-03 00:01:10 +0200

    Add Conjured item + test case

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,65 @@
 package com.gildedrose;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * General comment...
+ *
+ * The original code is a nested mess.  Here is a version that is much easier to extend without
+ * introducing side effects.
+ */
+
 class GildedRose {
     Item[] items;
+
+    private static final Map<String, ItemUpdater> ITEM_UPDATERS = new HashMap<>();
+    private static final ItemUpdater DEFAULT_ITEM_UPDATER;
+
+    static {
+      ITEM_UPDATERS.put("Aged Brie", item -> {
+        changeQuality(1, item);
+
+        if(item.sellIn <= 0) {
+          changeQuality(1, item);  // Possible bug in original, brie increases twice as fast in quality after sell date passed
+        }
+
+        item.sellIn--;
+      });
+
+      ITEM_UPDATERS.put("Sulfuras, Hand of Ragnaros", item -> {});
+
+      ITEM_UPDATERS.put("Backstage passes to a TAFKAL80ETC concert", item -> {
+        if(item.sellIn <= 0) {
+          item.quality = 0;
+        }
+        else {
+          changeQuality(item.sellIn <= 5 ? 3 : item.sellIn <= 10 ? 2 : 1, item);
+        }
+
+        item.sellIn--;
+      });
+
+      ITEM_UPDATERS.put("Conjured", item -> {
+        changeQuality(-2, item);
+
+        if(item.sellIn <= 0) {
+          changeQuality(-2, item);
+        }
+
+        item.sellIn--;
+      });
+
+      DEFAULT_ITEM_UPDATER = item -> {
+        changeQuality(-1, item);
+
+        if(item.sellIn <= 0) {
+          changeQuality(-1, item);
+        }
+
+        item.sellIn--;
+      };
+    }
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -9,54 +67,21 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+          ItemUpdater itemUpdater = ITEM_UPDATERS.get(items[i].name);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+          if(itemUpdater == null) {
+            itemUpdater = DEFAULT_ITEM_UPDATER;
+          }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+          itemUpdater.updateItem(items[i]);
         }
+    }
+
+    private interface ItemUpdater {
+      void updateItem(Item item);
+    }
+
+    private static void changeQuality(int delta, Item item) {
+      item.quality = Math.max(0, Math.min(50, item.quality + delta));
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,106 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public void shouldPassTestCases() {
+      // Functional versions of the desired behaviour:
+      List<TestCase> testCases = Arrays.asList(
+        new TestCase(
+          "Sulfuras, Hand of Ragnaros",
+          (q, sellIn, days) -> q,
+          (sellIn, days) -> sellIn
+        ),
+        new TestCase(
+          "Aged Brie",
+          (q, sellIn, days) -> Math.min(50, q + days + (days > sellIn ? days - sellIn : 0)),
+          (sellIn, days) -> sellIn - days
+        ),
+        new TestCase(
+          "Backstage passes to a TAFKAL80ETC concert",
+          (q, sellIn, days) -> {
+            int daysToGo = sellIn - days;
+            int daysPassedBelow10 = daysToGo <= 10 ? Math.min(10, sellIn) - daysToGo : 0;
+            int daysPassedBelow5 = daysToGo <= 5 ? Math.min(5, sellIn) - daysToGo : 0;
+
+            return daysToGo < 0 ? 0 : Math.min(50, q + days + daysPassedBelow10 + daysPassedBelow5);
+          },
+          (sellIn, days) -> sellIn - days
+        ),
+        new TestCase(
+          "Regular Item",
+          (q, sellIn, days) -> Math.max(0, q - days - Math.max(0, days - sellIn)),
+          (sellIn, days) -> sellIn - days
+        ),
+        new TestCase(
+          "Conjured",
+          (q, sellIn, days) -> Math.max(0, q - (days + Math.max(0, days - sellIn)) * 2),
+          (sellIn, days) -> sellIn - days
+        )
+      );
+
+      // Tests everything with upto 99 sell days and 0 to 50 quality:
+      for(int startSellIn = 0; startSellIn < 100; startSellIn++) {
+        for(int startQuality = 0; startQuality <= 50; startQuality++) {
+          for(TestCase testCase : testCases) {
+            String testCaseName = testCase.name + ", startQ=" + startQuality + ", startSellIn=" + startSellIn;
+            Item item = new Item(testCase.name, startSellIn, startQuality);
+
+            GildedRose rose = new GildedRose(new Item[] { item });
+
+            for(int daysPassed = 0; daysPassed < 100; daysPassed++) {
+              assertEquals(
+                "Quality incorrect for [" + testCaseName + "] after " + daysPassed + " days passed",
+                testCase.expectedQuality.afterDays(startQuality, startSellIn, daysPassed),
+                item.quality
+              );
+
+              assertEquals(
+                "SellIn incorrect for [" + testCaseName + "] after " + daysPassed + " days passed",
+                testCase.expectedSellIn.afterDays(startSellIn, daysPassed),
+                item.sellIn
+              );
+
+
+              rose.updateQuality();
+            }
+          }
+        }
+      }
     }
 
+    @FunctionalInterface
+    private interface ExpectedQuality {
+      /**
+       * Returns expected quality after the given number of days.
+       */
+      int afterDays(int quality, int sellIn, int days);
+    }
+
+    @FunctionalInterface
+    private interface ExpectedSellIn {
+      /**
+       * Returns expected sell-in time after the given number of days.
+       */
+      int afterDays(int sellIn, int days);
+    }
+
+    private static class TestCase {
+      private final String name;
+      private final ExpectedQuality expectedQuality;
+      private final ExpectedSellIn expectedSellIn;
+
+      public TestCase(String itemName, ExpectedQuality expectedQuality, ExpectedSellIn expectedSellIn) {
+        this.name = itemName;
+        this.expectedQuality = expectedQuality;
+        this.expectedSellIn = expectedSellIn;
+      }
+    }
 }
